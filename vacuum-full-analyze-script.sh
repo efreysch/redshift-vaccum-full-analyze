ec878002b573eee1999946bdc599f6728e073557
Erik Freyschuss <97680279+efreysch@users.noreply.github.com>
2023-12-15 10:03:30 +0100

    Update vacuum-full-analyze-script.sh

--- vacuum-full-analyze-script.sh
+++ vacuum-full-analyze-script.sh
@@ -1,5 +1,5 @@
 # version 1.0
-# Developed by: Erik Freyschuss (inspiration from TheDataGuy)
+# Developed by: Erik Freyschuss (inspiration from TheDataGuy - Copyright (c) 2020 Bhuvanesh)
 
 # Set some default values for all the variables
 host=unset
